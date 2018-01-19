6e2348d8b48bda81c606165643e1f1d20dfafad6
Daniel, Seeberger <daniel.seeberger@tecalliance.net>
2018-01-19 10:15:34 +0100

    First Upload

---
+++ InvasionOfAldebaran/AppBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Dynamic;
+using System.Windows;
+using Caliburn.Micro;
+using InvasionOfAldebaran.ViewModels;
+
+
+namespace InvasionOfAldebaran
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            this.Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            
+            this.DisplayRootViewFor<MainWindowViewModel>();
+        }
+    }
+}
---
+++ InvasionOfAldebaran/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace InvasionOfAldebaran.ViewModels
+{
+    class MainWindowViewModel : Conductor<Screen>.Collection.OneActive
+    {
+
+    }
+}
