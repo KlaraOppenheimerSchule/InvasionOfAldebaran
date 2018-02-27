22dcbaf3c1f2db111a9029ed0cf98c4d82d7c60a
Daniel, Seeberger <daniel.seeberger@tecalliance.net>
2018-02-27 09:12:40 +0100

    First Commit, Fixed naming bug

--- InvasionOfAldebaran/Views/MenuView.xaml.cs
+++ InvasionOfAldebaran/Views/MenuView.xaml.cs
@@ -18,9 +18,9 @@ namespace InvasionOfAldebaran.Views
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class MenuView : UserControl
     {
-        public UserControl1()
+        public MenuView()
         {
             InitializeComponent();
         }
