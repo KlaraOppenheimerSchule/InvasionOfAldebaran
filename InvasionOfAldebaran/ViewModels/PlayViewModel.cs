a772bb65cc3736f9d905c9bcd2604a66dd2c00ef
Daniel, Seeberger <daniel.seeberger@tecalliance.net>
2018-05-09 12:01:24 +0200

    Timer Intervall verringert

--- InvasionOfAldebaran/ViewModels/PlayViewModel.cs
+++ InvasionOfAldebaran/ViewModels/PlayViewModel.cs
@@ -15,10 +15,10 @@ namespace InvasionOfAldebaran.ViewModels
     public sealed class PlayViewModel : NotifyPropertyChangedBase
     {
         private const int maxWave = 4;
-        private const int spawnInterval = 4;
+        private const int spawnInterval = 5;
         private const int questionStartTime = 8;
 	    private const int maxPoints = 100;
-		private const double timerInterval = 0.016;
+		private const double timerInterval = 0.013;
 
         private readonly FrameWindowViewModel _frameViewModel;
         private readonly DispatcherTimer _timer = new DispatcherTimer();
@@ -126,14 +126,14 @@ namespace InvasionOfAldebaran.ViewModels
             if (_currentWave >= maxWave)
                 _spawnAllowed = false;
 
-	  //      if (this.Points >= maxPoints)
-	  //      {
-			//	var result = MessageBox.Show("You`ve won!", "Congratulations", MessageBoxButton.OK);
-		 //       if (result.Equals(MessageBoxResult.OK))
-			//        this.EndGame();
-			//}
-				
-            if (_spawnAllowed && _nextpSpawn <= DateTime.Now)
+			if (this.Points >= maxPoints)
+			{
+				var result = MessageBox.Show("You`ve won!", "Congratulations", MessageBoxButton.OK);
+				if (result.Equals(MessageBoxResult.OK))
+					this.EndGame();
+			}
+
+			if (_spawnAllowed && _nextpSpawn <= DateTime.Now)
             {
                 var enemies = _spawner.SpawnEnemies(this.CurrentQuestion);
                 _objects.AddRange(enemies);
@@ -258,13 +258,13 @@ namespace InvasionOfAldebaran.ViewModels
             _objects.Add(this.Player);
             this.CurrentQuestion = _spawner.GetQuestion();
             // First Spawn after this amount of seconds
-            _nextpSpawn = DateTime.Now.AddSeconds(5);
+            _nextpSpawn = DateTime.Now.AddSeconds(spawnInterval);
             this.CurrentWave = 0;
             _spawnAllowed = true;
             this.Points = 0;
             this.Message = "Shoot the wrong answers!";
 
-            _timer.Interval = TimeSpan.FromSeconds(0.016);
+            _timer.Interval = TimeSpan.FromSeconds(timerInterval);
             _timer.Start();
         }
 
